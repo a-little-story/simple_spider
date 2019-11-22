06f4c1588febd67b3cda3ef34958fd7174ec4322
a-little-story <zh1023135024@gmail.com>
2019-11-22 15:58:53 +0800

    add data

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-
-
-if __name__ == "__main__":
-    pass
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"}
+pre_url = "https://www.baidu.com/s?wd="
+next_url = "&si=movie.douban.com&ct=2097152"
+extract_tag = "bds.comm.iaurl"
+extract_tag_len = len(extract_tag)
--- spider_baidu_query.py
+++ spider_baidu_query.py
@@ -6,15 +6,15 @@ import json
 # from multiprocessing import Pool
 # import config_file
 # import multiprocessing as mp
+from config import *
 
 
-
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
-"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"}
-a = requests.get("https://www.baidu.com/s?wd=复仇者联盟&si=movie.douban.com&ct=2097152", headers = headers)
+headers = HEADERS
+query_words = '复仇者联盟'
+a = requests.get(pre_url+query_words+next_url, headers = headers)
 context = a.content.decode('utf8').split()
 for line in context:
-    if "bds.comm.iaurl" == line[:14]:
+    if line[:extract_tag_len] == extract_tag:
         print(line)
 
 
