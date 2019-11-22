0ae6223402067aa6d30053f43d2b2572e0bb5c86
a-little-story <zh1023135024@gmail.com>
2019-11-22 17:27:01 +0800

    to be completed

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"}
 pre_url = "https://www.baidu.com/s?wd="
 next_url = "&si=movie.douban.com&ct=2097152"
-extract_tag = "bds.comm.iaurl"
+extract_tag = "bds.comm.iaurl="
 extract_tag_len = len(extract_tag)
+pool_size = 10
+query_file = 'data/json_format'
+MAX_LENGTH = 38
--- spider_baidu_query.py
+++ spider_baidu_query.py
@@ -1,29 +1,63 @@
 #-*- encoding:utf-8 -*-
 import requests
 import time
-import sys
+import os
 import json
-# from multiprocessing import Pool
+
+from pprint import pprint
+from multiprocessing import Pool
 # import config_file
-# import multiprocessing as mp
+import multiprocessing as mp
 from config import *
 
 
-headers = HEADERS
-query_words = '复仇者联盟'
-a = requests.get(pre_url+query_words+next_url, headers = headers)
-context = a.content.decode('utf8').split()
-for line in context:
-    if line[:extract_tag_len] == extract_tag:
-        print(line)
+
+
+def extract_tag_content(query_words, pre_url=pre_url, next_url=next_url, headers=HEADERS,
+                extract_tag=extract_tag, extract_tag_len=extract_tag_len):
+    url = pre_url + query_words + next_url
+    a = requests.get(url, headers=headers)
+    context = a.content.decode('utf8')
+    with open(query_words, 'w', encoding='utf-8') as f:
+        f.write(context)
+    context = context.split()
+    print(len(context))
+    for line in context:
+        if line[:extract_tag_len] == extract_tag:
+            return line[extract_tag_len:]
+
+def spider_single(file_path, output_path):
+    with open(file_path, 'r', encoding='utf8') as f, \
+            open(output_path, 'w', encoding='utf8') as f_out:
+        for line in f:
+            query = extract_query(line)
+            result = extract_tag_content(query)
+            t = {}
+            t[query] = result
+            t = json.dumps(t, ensure_ascii=False) + '\n'
+            f_out.write(t)
 
 
 
+def extract_query(line):
+    line = json.loads(line)
+    return line['query'][:MAX_LENGTH]
 
-with open('test.txt', 'w', encoding='utf8') as f:
-    f.write(a.content.decode('utf8'))
+
+def split_file(file_path=query_file, nums=pool_size):
+    result  = os.popen(f'wc -l {file_path}').readlines()
+    print(result)
+    line_nums = int(result[0].split()[0])
+    nums_per_file = int(line_nums/nums) + 1
+    command = f"split -l {nums_per_file} {file_path} -d -a 2 {file_path}"
+    print(command)
+    os.popen(command).readlines()
 
 
+def multi_p_spider(func=extract_tag_content, pool_size=pool_size):
+    my_pool = Pool(processes=pool_size)
+    for i in range(pool_size):
+        my_pool.apply_async()
 
-# if __name__ == "__main__":
-#     pass
+if __name__ == "__main__":
+    spider_single('./data/json_format1', './data/json_format1out')
